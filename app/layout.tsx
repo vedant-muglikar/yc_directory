1ba8830246a494d1ac7cab944bea0f6087f316c3
vedant-muglikar <vedantmuglikar1@gmail.com>
2025-09-11 21:39:13 +0530

    Sanity schema db setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,9 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import "easymde/dist/easymde.min.css";
 import localFont from "next/font/local";
+import { isObjectInputProps } from "sanity";
 
 const workSans = localFont({
   src: [
